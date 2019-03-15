fcbfb8dda9fcc6a58ebce2a7a99fbbdac16e1e52
pcaserta <patrick.caserta@live.longwood.edu>
2019-03-15 19:44:57 -0400

    working descent layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     var questions = [{
 
         question: "In which game did Mario first appear?",
-        choices: ["donkey kong", "supemario brothers", "mario brothers", "yoshis world"],
+        choices: ["Donkey Kong", "Supemario Bros", "Mario brothers", "Yoshis World"],
     }, {
         question: "Which damsel did Mario rescue first?",
         choices: ["Peach", "Pauline", "Daisy", "Zelda"],
@@ -16,7 +16,7 @@ $(document).ready(function () {
         question: "Who is Mario's brother?",
         choices: ["Wauligi", "Wario", "Luigi", "Peach"]
     },{
-        question: "What comapny invented Mario",
+        question: "What comapny invented Mario?",
         choices: ["Nintendo", "NES", "SONY", "Activision"]
     },{
         question: "Before Mario what his original name?",
@@ -26,14 +26,14 @@ $(document).ready(function () {
         choices: ["Birdo", "Yoshi", "Godzilla", "Captain Krool"]
     },{
         question: "What is the fastest completeion of Mario 64?",
-        choices: ["1 hour, 15 min and 15 seconds", "1 hour, 15 min and 30 seconds", "1 hour, 25 min and 45 seconds", "2 hour, 25 min and 13 seconds"]
+        choices: ["1 hour and 15 mins", "1 hour and 18 mins", "1 hour and 25 mins", "2 hour and 25 mins "]
     },
 
     
     ]
 
     //timer variable
-    var time = 9999
+    var time = 30
     var correct = 0
     var incorrect = 0
     notGuessed = 0
@@ -48,7 +48,7 @@ $(document).ready(function () {
     })
     //decrements the time by 1 and puts it on html
     function count() {
-        $(".timer").text(time);
+        $(".timer").text( "Time: " +time);
         time--
         if (time === -1) {
             gameover()
@@ -56,19 +56,19 @@ $(document).ready(function () {
         console.log(time)
     }
 
-
+    //need to fix this mess***//
     //displays question
     function displayQuestion() {
         //question1
         $(".question").append("<p class ='q1' >" + questions[0].question + "</p>")
-        $(".input-group").append("<div class='input-group-prepend'> <div class='input-group-text rounded-0'> <input type='radio' name='one'  value='0'><p class='anwsers' id='anwser1'></p><input type='radio' name='one' value='1'><p class='anwsers' id='anwser2'></p><input type='radio' name='one'  value='1'><p class='anwsers' id='anwser3'></p><input type='radio' name ='one' value='1'> <p class='anwsers' id='anwser4'></p></div> </div>")
+        $(".input-group").append("<div class='input-group-prepend'> <div class='input-group-text rounded-0 bg-white'> <input type='radio' name='one'  value='0'><p class='anwsers' id='anwser1'></p><input type='radio' name='one' value='1'><p class='anwsers' id='anwser2'></p><input type='radio' name='one'  value='1'><p class='anwsers' id='anwser3'></p><input type='radio' name ='one' value='1'> <p class='anwsers' id='anwser4'></p></div> </div>")
         $("#anwser1").text(questions[0].choices[0])
         $("#anwser2").text(questions[0].choices[1])
         $("#anwser3").text(questions[0].choices[2])
         $("#anwser4").text(questions[0].choices[3])
         //question2
         $(".question2").append("<p class ='q1' >" + questions[1].question + "</p>")
-        $(".input-group2").append(" <div class='input-group-prepend'>  <div class='input-group-text rounded-0'> <input type='radio' name='two'  value='1'><p class='anwsers' id='anwser5'></p><input type='radio' name='two' value='0'><p class='anwsers' id='anwser6'></p><input type='radio' name='two'  value='1'><p class='anwsers' id='anwser7'></p><input type='radio' name ='two' value='1'> <p class='anwsers' id='anwser8'></p></div> </div>")
+        $(".input-group2").append(" <div class='input-group-prepend'>  <div class='input-group-text rounded-0 bg-white'> <input type='radio' name='two'  value='1'><p class='anwsers' id='anwser5'></p><input type='radio' name='two' value='0'><p class='anwsers' id='anwser6'></p><input type='radio' name='two'  value='1'><p class='anwsers' id='anwser7'></p><input type='radio' name ='two' value='1'> <p class='anwsers' id='anwser8'></p></div> </div>")
         $("#anwser5").text(questions[1].choices[0])
         $("#anwser6").text(questions[1].choices[1])
         $("#anwser7").text(questions[1].choices[2])
@@ -76,7 +76,7 @@ $(document).ready(function () {
 
         //question3
         $(".question3").append("<p class ='q1' >" + questions[2].question + "</p>")
-        $(".input-group3").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0'> <input type='radio' name='three'  value='1'><p class='anwsers' id='anwser9'></p><input type='radio' name='three' value='1'><p class='anwsers' id='anwser10'></p><input type='radio' name='three'  value='1'><p class='anwsers' id='anwser11'></p><input type='radio' name ='three' value='0'> <p class='anwsers' id='anwser12'></p></div> </div>")
+        $(".input-group3").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0 bg-white'> <input type='radio' name='three'  value='1'><p class='anwsers' id='anwser9'></p><input type='radio' name='three' value='1'><p class='anwsers' id='anwser10'></p><input type='radio' name='three'  value='1'><p class='anwsers' id='anwser11'></p><input type='radio' name ='three' value='0'> <p class='anwsers' id='anwser12'></p></div> </div>")
         
         $("#anwser9").text(questions[2].choices[0])
         $("#anwser10").text(questions[2].choices[1])
@@ -85,7 +85,7 @@ $(document).ready(function () {
 
         //question4
         $(".question4").append("<p class ='q1' >" + questions[3].question + "</p>")
-        $(".input-group4").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0'> <input type='radio' name='four'  value='1'><p class='anwsers' id='anwser13'></p><input type='radio' name='four' value='1'><p class='anwsers' id='anwser14'></p><input type='radio' name='four'  value='0'><p class='anwsers' id='anwser15'></p><input type='radio' name ='four' value='1'> <p class='anwsers' id='anwser16'></p></div> </div>")
+        $(".input-group4").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0 bg-white'> <input type='radio' name='four'  value='1'><p class='anwsers' id='anwser13'></p><input type='radio' name='four' value='1'><p class='anwsers' id='anwser14'></p><input type='radio' name='four'  value='0'><p class='anwsers' id='anwser15'></p><input type='radio' name ='four' value='1'> <p class='anwsers' id='anwser16'></p></div> </div>")
         $("#anwser13").text(questions[3].choices[0])
         $("#anwser14").text(questions[3].choices[1])
         $("#anwser15").text(questions[3].choices[2])
@@ -93,7 +93,7 @@ $(document).ready(function () {
 
         //question5
         $(".question5").append("<p class ='q1' >" + questions[4].question + "</p>")
-        $(".input-group5").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0'> <input type='radio' name='five'  value='0'><p class='anwsers' id='anwser17'></p><input type='radio' name='five' value='1'><p class='anwsers' id='anwser18'></p><input type='radio' name='five'  value='1'><p class='anwsers' id='anwser19'></p><input type='radio' name ='five' value='1'> <p class='anwsers' id='anwser20'></p></div> </div>")
+        $(".input-group5").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0 bg-white'> <input type='radio' name='five'  value='0'><p class='anwsers' id='anwser17'></p><input type='radio' name='five' value='1'><p class='anwsers' id='anwser18'></p><input type='radio' name='five'  value='1'><p class='anwsers' id='anwser19'></p><input type='radio' name ='five' value='1'> <p class='anwsers' id='anwser20'></p></div> </div>")
         $("#anwser17").text(questions[4].choices[0])
         $("#anwser18").text(questions[4].choices[1])
         $("#anwser19").text(questions[4].choices[2])
@@ -101,7 +101,7 @@ $(document).ready(function () {
 
         //question6
         $(".question6").append("<p class ='q1' >" + questions[5].question + "</p>")
-        $(".input-group6").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0'> <input type='radio' name='six'  value='0'><p class='anwsers' id='anwser21'></p><input type='radio' name='six' value='1'><p class='anwsers' id='anwser22'></p><input type='radio' name='six'  value='1'><p class='anwsers' id='anwser23'></p><input type='radio' name ='six' value='1'> <p class='anwsers' id='anwser24'></p></div> </div>")
+        $(".input-group6").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0 bg-white'> <input type='radio' name='six'  value='0'><p class='anwsers' id='anwser21'></p><input type='radio' name='six' value='1'><p class='anwsers' id='anwser22'></p><input type='radio' name='six'  value='1'><p class='anwsers' id='anwser23'></p><input type='radio' name ='six' value='1'> <p class='anwsers' id='anwser24'></p></div> </div>")
 
         $("#anwser21").text(questions[5].choices[0])
         $("#anwser22").text(questions[5].choices[1])
@@ -109,7 +109,7 @@ $(document).ready(function () {
         $("#anwser24").text(questions[5].choices[3])
 //question7
         $(".question7").append("<p class ='q1' >" + questions[6].question + "</p>")
-        $(".input-group7").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0'> <input type='radio' name='seven'  value='1'><p class='anwsers' id='anwser25'></p><input type='radio' name='seven' value='0'><p class='anwsers' id='anwser26'></p><input type='radio' name='seven'  value='1'><p class='anwsers' id='anwser27'></p><input type='radio' name ='seven' value='1'> <p class='anwsers' id='anwser28'></p></div> </div>")
+        $(".input-group7").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0 bg-white'> <input type='radio' name='seven'  value='1'><p class='anwsers' id='anwser25'></p><input type='radio' name='seven' value='0'><p class='anwsers' id='anwser26'></p><input type='radio' name='seven'  value='1'><p class='anwsers' id='anwser27'></p><input type='radio' name ='seven' value='1'> <p class='anwsers' id='anwser28'></p></div> </div>")
         
         $("#anwser25").text(questions[6].choices[0])
         $("#anwser26").text(questions[6].choices[1])
@@ -118,7 +118,7 @@ $(document).ready(function () {
 
         //question 8
         $(".question8").append("<p class ='q1' >" + questions[7].question + "</p>")
-        $(".input-group8").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0'> <input type='radio' name='eight'  value='0'><p class='anwsers' id='anwser29'></p><input type='radio' name='eight' value='1'><p class='anwsers' id='anwser30'></p><input type='radio' name='eight'  value='1'><p class='anwsers' id='anwser31'></p><input type='radio' name ='eight' value='1'> <p class='anwsers' id='anwser32'></p></div> </div>")
+        $(".input-group8").append(" <div class='input-group-prepend'> <div class='input-group-text rounded-0 bg-white'> <input type='radio' name='eight'  value='0'><p class='anwsers' id='anwser29'></p><input type='radio' name='eight' value='1'><p class='anwsers' id='anwser30'></p><input type='radio' name='eight'  value='1'><p class='anwsers' id='anwser31'></p><input type='radio' name ='eight' value='1'> <p class='anwsers' id='anwser32'></p></div> </div>")
         $("#anwser29").text(questions[7].choices[0])
         $("#anwser30").text(questions[7].choices[1])
         $("#anwser31").text(questions[7].choices[2])
@@ -269,9 +269,10 @@ $(document).ready(function () {
         clearInterval(intervalId)
         checkanwser()
         $('#letshide').hide()
-        $('#correct').append("Correct: " + correct)
-        $("#incorrect").append("Incorrect: " + incorrect)
-        $("#notGuessed").append("Not guessed: " + notGuessed)
+        $('body').prepend("<div class = 'final'> </div>")
+        $('.final').append("<p> Correct: " + correct + "</p>")
+        $(".final").append("<p>Incorrect: " + incorrect +"</p>")
+        $(".final").append("<p>Not guessed: " + notGuessed +"</p>" )
 
     }
 })
